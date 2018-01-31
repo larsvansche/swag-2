fb040e79221f54dba71e47dc62770b7781833e02
larsvs <lars.v.scheijndel@gmail.com>
2018-01-31 15:36:32 +0100

    changed filenames

--- server.py
+++ server.py
@@ -14,8 +14,8 @@ def accept_incoming_connections():
 
 def handle_client(client):  # Takes client socket as argument.
     """Handles a single client connection."""
-
     name = client.recv(BUFSIZ).decode("utf8")
+    print("Client entered: " + name)
     welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
     client.send(bytes(welcome, "utf8"))
     msg = "%s has joined the chat!" % name
