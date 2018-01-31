71262379e648e946ff59802ebbf2f2cc3c474bf7
ricardosnabel <31866767+ricardosnabel@users.noreply.github.com>
2018-01-31 17:09:27 +0100

    Joe

--- client.py
+++ client.py
@@ -2,7 +2,8 @@
 """Script for Tkinter GUI chat client."""
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
-import tkinter
+import time
+##import tkinter
 
 
 def receive():
@@ -10,7 +11,7 @@ def receive():
     while True:
         try:
             msg = client_socket.recv(BUFSIZ).decode("utf8")
-            msg_list.insert(tkinter.END, msg)
+##            msg_list.insert(tkinter.END, msg)
         except OSError:  # Possibly client has left the chat.
             break
 
@@ -18,43 +19,44 @@ def receive():
 def send(msg):  # event is passed by binders.
     """Handles sending of messages."""
     # msg = my_msg.get()
-    my_msg.set("")  # Clears input field.
+##    my_msg.set("")  # Clears input field.
     client_socket.send(bytes(msg, "utf8"))
     if msg == "{quit}":
         client_socket.close()
         top.quit()
-
+##    print(msg)
+    time.sleep(0.2)
 
 def on_closing(event=None):
     """This function is to be called when the window is closed."""
     my_msg.set("{quit}")
     send()
 
-top = tkinter.Tk()
-top.title("Chatter")
+##top = tkinter.Tk()
+##top.title("Chatter")
 
-messages_frame = tkinter.Frame(top)
-my_msg = tkinter.StringVar()  # For the messages to be sent.
-my_msg.set("Type your messages here.")
-scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
-# Following will contain the messages.
-msg_list = tkinter.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
-scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
-msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
-msg_list.pack()
-messages_frame.pack()
+##messages_frame = tkinter.Frame(top)
+##my_msg = tkinter.StringVar()  # For the messages to be sent.
+##my_msg.set("Type your messages here.")
+##scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
+### Following will contain the messages.
+##msg_list = tkinter.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
+##scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+##msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+##msg_list.pack()
+##messages_frame.pack()
 
-entry_field = tkinter.Entry(top, textvariable=my_msg)
-entry_field.bind("<Return>", send)
-entry_field.pack()
-send_button = tkinter.Button(top, text="Send", command=send)
-send_button.pack()
-
-top.protocol("WM_DELETE_WINDOW", on_closing)
+##entry_field = tkinter.Entry(top, textvariable=my_msg)
+##entry_field.bind("<Return>", send)
+##entry_field.pack()
+##send_button = tkinter.Button(top, text="Send", command=send)
+##send_button.pack()
+##
+##top.protocol("WM_DELETE_WINDOW", on_closing)
 
 #----Now comes the sockets part----
-HOST = input('Enter host: ')
-PORT = input('Enter port: ')
+HOST = '192.168.1.2'
+PORT = '1999'
 if not PORT:
     PORT = 1999
 else:
@@ -68,4 +70,4 @@ client_socket.connect(ADDR)
 
 receive_thread = Thread(target=receive)
 receive_thread.start()
-tkinter.mainloop()  # Starts GUI execution.
+##tkinter.mainloop()  # Starts GUI execution.
