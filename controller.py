b1dfacc1f72f430fe1fa0e8a556af6e369535074
ricardosnabel <31866767+ricardosnabel@users.noreply.github.com>
2018-02-05 07:09:40 +0100

    Update controller.py

--- controller.py
+++ controller.py
@@ -8,8 +8,6 @@ import client
 directions = []
 buttons = []
 
-#Y = MCP3008(1)
-#X = MCP3008(2)
 Pot = MCP3008(0)
 
 previouspotvalue = 0
@@ -62,10 +60,6 @@ class pushButton:
         buttonState = button.is_pressed
         return buttonState
 
-#yDir = joyStick(0, 'Y')
-#xDir = joyStick(1, 'X')
-#gButton = pushButton(2, 'green')
-
 
 def sendAllInput(directions, buttons):
     sendDict = {}
@@ -79,30 +73,12 @@ def sendAllInput(directions, buttons):
 
     return sendDict
 
-RedLed = LED(13)
-YellowLed = LED(19)
-GreenLed = LED(26)
-
 while True:
-##    print("jemoeder")
     detectedvalue = round(Pot.value, 2)
     if detectedvalue != previouspotvalue:
         client.send("tacPot1" + str(detectedvalue))
         previouspotvalue = detectedvalue
-    
-    # if previousinputstate1 != input_state1:
-    #     client.send("jemoeder")
-    #     previousinputstate1 = input_state1
-    # elif previousinputstate2 != input_state2:
-    #     client.send("tacinput2")
-    #     previousinputstate2 = input_state2
-    # elif previousinputstate3 != input_state3:
-    #     client.send("tacinput3")
-    #     previousinputstate3 = input_state3
-    # elif previousinputstate4 != input_state4:
-    #     client.send("tacinput4")
-    #     previousinputstate4 = input_state4
-    
+
     if not input_state1:
         client.send("tacinput1")
     if not input_state2:
@@ -111,58 +87,10 @@ while True:
         client.send("tacinput3")
     if not input_state4:
         client.send("tacinput4")
-
-    # if Pot.value > 0.67:
-    #     RedLed.off()
-    #     YellowLed.off()
-    #     GreenLed.on()
-    # elif Pot.value > 0.34:
-    #     RedLed.off()
-    #     YellowLed.on()
-    #     GreenLed.off()
-    # elif Pot.value < 0.339:
-    #     RedLed.on()
-    #     YellowLed.off()
-    #     GreenLed.off()
     
     input_state1 = gpio.input(21)
     input_state2 = gpio.input(20)
     input_state3 = gpio.input(16)
     input_state4 = gpio.input(12)
 
-    # if input_state1 == False:
-    #     print('Button1 Pressed')
-    #     time.sleep(0.2)
-    # elif input_state2 == False:
-    #     print('Button2 Pressed')
-    #     time.sleep(0.2)
-    # elif input_state3 == False:
-    #     print('Button3 Pressed')
-    #     time.sleep(0.2)
-    # elif input_state4 == False:
-    #     print('Button4 Pressed')
-    #     time.sleep(0.2)
-    # print('Y value ', Y.value)
-    # print('X value ', X.value)
-    # print('Pot value ', Pot.value)
-
-
     sleep(0.1)
-
-##while True:
-##    input_state1 = gpio.input(21)
-##    input_state2 = gpio.input(20)
-##    input_state3 = gpio.input(16)
-##    input_state4 = gpio.input(12)
-##    if input_state1 == False:
-##        print('Button1 Pressed')
-##        time.sleep(0.2)
-##    elif input_state2 == False:
-##        print('Button2 Pressed')
-##        time.sleep(0.2)
-##    elif input_state3 == False:
-##        print('Button3 Pressed')
-##        time.sleep(0.2)
-##    elif input_state4 == False:
-##        print('Button4 Pressed')
-##        time.sleep(0.2)
